cacb4036476c523fca3ff26a9e160e681f8423b8
unknown <souptikshivam@gmail.com>
2025-09-04 20:11:45 +0530

    claim

---
+++ src/app/api/claim/route.js
@@ -0,0 +1,15 @@
+import  challengesDb  from '../../db/challengesDb';
+
+export async function POST(req) {
+    // Receive the id of the challenge in the request body
+    const { id } = await req.json();
+    const stmt=challengesDb.prepare('UPDATE challenges SET clear = 1 WHERE id = ?');
+    // Update the 'clear' attribute to true for the specified challenge by id
+    
+    try {
+        stmt.run(id);
+        return new Response(JSON.stringify({ success: true }), { status: 201 });
+    } catch (error) {
+        return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
+    }
+}
